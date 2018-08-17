b3de9d593ad32a795457e48959756c3e8eca7006
Tom Hughes <tom@openobjects.com>
2018-08-17 20:56:46 +0100

    modded@ .

--- gegario/ansible.sh
+++ gegario/ansible.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt-get install ansible apache2 -y
+if [[ -f /var/www/html/index.html ]]; then sudo rm /var/www/html/index.html; fi
+sudo mkdir /var/www/html/css -p
+sudo wget "https://s3-eu-west-1.amazonaws.com/helloawscode/css/main.css" -O /var/www/html/css/main.css
+sudo wget "https://s3-eu-west-1.amazonaws.com/helloawscode/index.php" -O /var/www/html/index.php
+
